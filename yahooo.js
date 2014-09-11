aedbe801586390f81ebf7b7dbe2e364e3a85aa7e
parkjiyun <jyun38@gmail.com>
2014-09-11 11:19:53 +0900

    llkkyiorw

---
+++ yahooo.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(640, 480);
+  background(0);
+  stroke(255);
+  noFill();
+
+  line(10, 10, 90, 10); // x1, y1, x2, y2
+    rect(20, 20, 80, 50); // x, y, width, height
+    ellipse(50, 50, 50, 50); // centerX, centerY, width, height
+}
